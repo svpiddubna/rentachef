481264f968966487536504e7f39dcde6908c77be
Roger Dong <r.dong90@gmail.com>
2019-02-25 18:19:15 +0100

    added basic User model validations, created User controller with 'Show' method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
+  validates :first_name, uniqueness: true, presence: true
+  validates :last_name, uniqueness: true, presence: true
+  validates :email, presence: true
 end
