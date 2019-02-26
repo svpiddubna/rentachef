c4b4c613ad94c167280612eb9699bbe32e17036f
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-02-26 10:23:30 +0100

    Chef index, chef show

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,10 @@
+class ChefsController < ApplicationController
+  def index
+    @chefs = Chef.where(location: params[:location])
+  end
+
+  def show
+    @chef = Chef.find(params[:id])
+  end
+
+end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,6 @@
 class Chef < ApplicationRecord
+  has_many :bookings
+  has_many :users, through: :bookings
+  validates :first_name, :last_name, presence: true
+  validates :location, :rate, :cuisine, presence: true
 end
