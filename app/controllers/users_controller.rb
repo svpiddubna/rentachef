8ef7a46d392ad7d703d3664132919b114235df04
Roger Dong <r.dong90@gmail.com>
2019-02-26 15:46:31 +0100

    allow display of upcoming and past bookings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @booking = Booking.new
+    @bookings = current_user.bookings
+    @upcoming_bookings = @bookings.where("date > '#{Date.current}'")
+    @past_bookings = @bookings.where("date < '#{Date.current}'")
   end
-
 end
