72c173b4eca8c2d9c285383343882d618dde7683
Roger Dong <r.dong90@gmail.com>
2019-02-26 17:11:28 +0100

    redesigned user controller to become a dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
-class UsersController < ApplicationController
+class DashboardController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @bookings = current_user.bookings
+    @user = current_user
+    # @booking = Booking.find(params[:id])
+    @bookings = @user.bookings
     @upcoming_bookings = @bookings.where("date > '#{Date.current}'")
     @past_bookings = @bookings.where("date < '#{Date.current}'")
   end
